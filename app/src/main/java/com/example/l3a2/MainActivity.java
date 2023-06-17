5b124693dde4e85dde6654ffec5eb35f3562c98c
ART2010311 <induthakur78687@gmail.com>
2023-06-17 11:43:03 +0545

    Initial commit

---
+++ app/src/main/java/com/example/l3a2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.l3a2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"How are you",Toast.LENGTH_LONG).show();
+            }
+
+
+        });
+    }
+}
